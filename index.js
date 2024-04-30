fb2334c9686252c2aa6f04e3404444aad08050ce
zhuyingames <zhuyingames@proton.me>
2024-04-30 14:14:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const canvas = document.getElementById("canvas");
-const context2D = canvas.getContext("2d");
+const file = document.getElementById("file");
+const origin = document.getElementById("origin");
+const x = document.getElementById("x");
+const y = document.getElementById("y");
+const w = document.getElementById("w");
+const h = document.getElementById("h");
+const cropped = document.getElementById("cropped");
+const download = document.getElementById("download");
+
+const context2D = origin.getContext("2d");
